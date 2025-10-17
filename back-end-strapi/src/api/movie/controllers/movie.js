7a8285c11eb1c27d2ae7f7b6e257dc27b44712d0
Raphael <raphael-anelli38@live.fr>
2025-10-17 12:26:15 +0200

    Modification controllers et routes

--- back-end-strapi/src/api/movie/controllers/movie.js
+++ back-end-strapi/src/api/movie/controllers/movie.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::movie.movie');
+module.exports = createCoreController('api::movie.movie', ({ strapi }) => ({
+
+  // Endpoint custom servant à synchroniser les films depuis TMDb
+  async syncFrenchMovies(ctx) {
+    // try {
+    //   const count = await strapi
+    //     .service('api::movie.movie')
+    //     .syncFrenchMoviesFrom2000();
+
+    //   ctx.send({ message: `Synchronisation réussie : ${count} films importés.` });
+    // } catch (error) {
+    //   console.error('Erreur de synchronisation :', error);
+    //   ctx.internalServerError('Erreur pendant la synchronisation');
+    // }
+    ctx.send({ message: 'Test ok' });
+  },
+}));
--- back-end-strapi/src/api/movie/routes/movie.js
+++ back-end-strapi/src/api/movie/routes/movie.js
@@ -7,3 +7,17 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::movie.movie');
+
+// module.exports = {
+//   routes: [
+//     {
+//       method: 'POST',
+//       path: '/movies/sync-french',
+//       handler: 'movie.syncFrenchMovies',
+//       config: {
+//         auth: false,
+//       },
+//     },
+//   ],
+// };
+
